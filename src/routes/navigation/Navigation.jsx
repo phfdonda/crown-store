3e3e60e972905270570fe892452c40a69baa3ad3
phfdonda <phfdonda@gmail.com>
2024-04-23 12:20:28 -0300

    Update navbar

---
+++ src/routes/navigation/Navigation.jsx
@@ -0,0 +1,27 @@
+import { Link, Outlet } from "react-router-dom"
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss'
+
+function Navigation() {
+    return (
+        <>
+            <nav className="navigation">
+                <Link to="/" className="logo_container"><CrwnLogo /></Link>
+                <ul className="nav_links_container">
+                    <li className="nav_link">
+                        <Link to="/shop" >Shop</Link>
+                    </li> <li className="nav_link">
+                        <Link to="/shop" >Shop</Link>
+                    </li> <li className="nav_link">
+                        <Link to="/shop" >Shop</Link>
+                    </li> <li className="nav_link">
+                        <Link to="/shop" >Shop</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </>
+    )
+}
+export default Navigation
